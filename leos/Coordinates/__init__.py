92c1a5292c424f4f06fb4930f837278a89736367
Srikrishna Karthick <srikrishnakarthick108@gmail.com>
2026-06-17 16:00:22 +0000

    feat(coordinates): establish sorted public api namespace for coordinates submodule

--- leos/Coordinates/__init__.py
+++ leos/Coordinates/__init__.py
@@ -1,25 +1,44 @@
-from .geometry import Geometry
+"""
+LEOS: Coordinates and Astro-Geometry Submodule
+Exposes SPICE ephemeris access utilities, reference frame transformations,
+and high-fidelity solar illumination metrics.
+"""
+
+from .geometry import Geometry, SUPPORTED_BODIES
+from .solar_geometry import (
+    illumination_fraction,
+    solar_zenith_angle,
+    subsolar_point,
+    sun_body_distance,
+    toa_irradiance,
+)
 from .spice_utils import (
-    discover_kernels,
-    load_kernels,
-    unload_kernels,
-    kernel_sandbox,
-    utc_to_et,
-    et_to_utc,
     body_name_to_id,
     body_radii,
+    discover_kernels,
+    et_to_utc,
+    kernel_sandbox,
+    load_kernels,
     sun_position,
+    unload_kernels,
+    utc_to_et,
 )
 
 __all__ = [
     "Geometry",
-    "discover_kernels",
-    "load_kernels",
-    "unload_kernels",
-    "kernel_sandbox",
-    "utc_to_et",
-    "et_to_utc",
+    "SUPPORTED_BODIES",
     "body_name_to_id",
     "body_radii",
+    "discover_kernels",
+    "et_to_utc",
+    "illumination_fraction",
+    "kernel_sandbox",
+    "load_kernels",
+    "solar_zenith_angle",
+    "subsolar_point",
+    "sun_body_distance",
     "sun_position",
+    "toa_irradiance",
+    "unload_kernels",
+    "utc_to_et",
 ]

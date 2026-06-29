791175ae40bedf2731bb3921c182430eaf89d1d7
Srikrishna Karthick <srikrishnakarthick108@gmail.com>
2026-06-29 16:22:43 +0000

    Add docstring structural contracts to missions package init

--- leos/kernels/missions/__init__.py
+++ leos/kernels/missions/__init__.py
@@ -12,6 +12,7 @@ fetch_kernels.py dispatch through one MISSION_RESOLVERS table instead of
 special-casing each mission.
 
 To add a new mission:
-  1. Create fetch_<name>_kernels.py here with a get_kernel_urls() function.
+  1. Create fetch_<name>_kernels.py here (or under a planet subpackage like
+     Mars/) with a get_kernel_urls() function.
   2. Import it in fetch_kernels.py and add one line to MISSION_RESOLVERS.
 """
